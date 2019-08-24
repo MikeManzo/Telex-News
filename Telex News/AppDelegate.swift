ab63737382d5454e1d1ccad4d78b16a7803a79c6
Mike Manzo <manzo.mike@gmail.com>
2019-08-24 12:19:06 -0400

    Initial Commit

---
+++ Telex News/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Telex News
+//
+//  Created by Mike Manzo on 8/24/19.
+//  Copyright © 2019 Quantum Joker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Telex News/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Telex News
+//
+//  Created by Mike Manzo on 8/24/19.
+//  Copyright © 2019 Quantum Joker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
